a98cf234100957deaaf472cc215ba17d8d7f38da
Donovan Lange <dolange@microsoft.com>
2016-06-30 15:58:52 +0200

    Chocolatey package to install the Nuget VSS CredentialProvider in your global Nuget directory

---
+++ Nuget.CredentialProvider.VSS/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName   = 'Nuget.CredentialProvider.VSS'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url           = 'https://www.nuget.org/api/v2/package/Microsoft.VisualStudio.Services.NuGet.CredentialProvider/0.14.0'
+$installDir    = Join-Path $env:LOCALAPPDATA "Nuget\CredentialProviders"
+
+## Download and unpack the zip file:
+$downloadedZip = Join-Path $toolsDir 'Nuget.CredentialProvider.VSS.zip'
+Get-ChocolateyWebFile "$packageName" "$downloadedZip" "$url"
+Get-ChocolateyUnzip "$downloadedZip" "$toolsDir"
+
+Copy-Item "$toolsDir\tools\CredentialProvider.VSS.exe" "$installDir" -Force
