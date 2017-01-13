21537d3b4486bb6469f94fc4de6bc4838dc85dba
Donovan Lange <dolange@microsoft.com>
2017-01-12 16:55:44 -0800

    Update to version 0.23 of the Microsoft.VisualStudio.Services.NuGet.CredentialProvider package, and bump our choco package accordingly

--- Nuget.CredentialProvider.VSS/tools/chocolateyinstall.ps1
+++ Nuget.CredentialProvider.VSS/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@
 
 $packageName   = 'Nuget.CredentialProvider.VSS'
 $toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url           = 'https://www.nuget.org/api/v2/package/Microsoft.VisualStudio.Services.NuGet.CredentialProvider/0.14.0'
+$url           = 'https://www.nuget.org/api/v2/package/Microsoft.VisualStudio.Services.NuGet.CredentialProvider/0.23.0'
 $installDir    = Join-Path $env:LOCALAPPDATA "Nuget\CredentialProviders"
 
 ## Download and unpack the zip file:
 $downloadedZip = Join-Path $toolsDir 'Nuget.CredentialProvider.VSS.zip'
-Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$downloadedZip" -Url "$url" -Checksum "AA79AE37517A15F0807A19A6D68077188DEB2DB5CD0D886EDFC767AB5EACBB29" -ChecksumType "sha256"
+Get-ChocolateyWebFile -PackageName "$packageName" -FileFullPath "$downloadedZip" -Url "$url" -Checksum "1D59368C45A2163F75D53BEC565915DFFE0A3C2EF35DE6466570CFE502503274" -ChecksumType "sha256"
 Get-ChocolateyUnzip "$downloadedZip" "$toolsDir"
 
 if (!(Test-Path -Path "$installDir"))
